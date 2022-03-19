c58f7600918266bc5d8e3146bef0703e67689fa8
khanshorif331 <khanshorif331@gmail.com>
2022-03-19 14:50:29 +0600

    bug fixed of the battery full error

--- src/Mobile/Mobile.js
+++ src/Mobile/Mobile.js
@@ -5,23 +5,30 @@ const Mobile = () => {
     const [battery,setBattery]=useState(100)
 
     const batteryDataDown=()=>{
+        document.getElementById('full').style.display='none'
         const newBatteryData =battery-10
         if(newBatteryData<0){
             return
         }
         setBattery(newBatteryData)
     }
-    const batteryDataUp=()=>{
+    const batteryDataUp=()=>{ 
         const newBatteryData =battery+10
+        
+        
         if(newBatteryData>100){
-            return
+            return(
+                document.getElementById('full').style.display='block'
+            )
         }
         setBattery(newBatteryData)
     }
     return (
         <div>
+            <h1>Charging Battery</h1>
             <img width={50} src={logo} alt="" />
-            <h1>{battery}%</h1>
+            <h1>Charging on Battery: {battery}%</h1>
+            <p id='full' style={{display:'none'}}>Battery Full</p>
             <button onClick={batteryDataDown}>Battery Down</button>
             <button onClick={batteryDataUp}>Battery Up</button>
         </div>
